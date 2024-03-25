406d000794ff6406a84f82f4e9f7365b72f3cb68
CarlosAugustoP <capv2004@gmail.com>
2024-03-25 12:15:39 -0300

    fixing thing

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -35,7 +35,7 @@ export default function Header() {
     width: '100%',
     display: 'flex',
     flexDirection: 'column',
-    height: '12vh',
+    height: '100px',
     // Remove position: 'absolute', top, left, right, zIndex properties
   };
   return (
