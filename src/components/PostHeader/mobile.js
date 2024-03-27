89cc53861f938aa5c4fe77b605484b53040f2810
Gabriel Pires <gabrielpires2991@gmail.com>
2024-03-26 21:20:47 -0300

    allPosts in Caroussel

--- src/components/PostHeader/mobile.js
+++ src/components/PostHeader/mobile.js
@@ -1,13 +1,15 @@
 
 import React from "react"
-import {posts} from './posts';
+import {posts, posts2, posts3} from './posts';
 import { CarousselContainer, CarousselItem } from "./styles";
+
+const allPosts = [...posts, ...posts2, ...posts3];
 
 export const Mobile = () => {
     return (
         <div>
             <CarousselContainer>
-                {posts.map((post, index) => (
+                {allPosts.map((post, index) => (
                     <CarousselItem key={index}>
                         <img src={post.image} alt={post.title} />
                         <strong>{post.title}</strong>
