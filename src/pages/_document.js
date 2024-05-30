35460d880d32722d36d028ecdb04e6574312649a
Carlos Augusto <capv2004@gmail.com>
2024-05-30 17:15:32 -0300

    style: add title

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -8,6 +8,7 @@ export default function Document() {
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
       <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"/>
       <link rel="icon" href="favicon.ico" type="image/x-icon"></link>
+      <title>Construtora P&V</title>
       </Head>
       <body>
         <Main />
